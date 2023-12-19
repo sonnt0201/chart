0ceea62a31f51ae6e6ed0d9a0ce41a3ccbf6f902
SonNguyen <nguyenthaison0201@gmail.com>
2023-12-19 21:33:28 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import Chart from "chart.js/auto";
+import { Line } from 'react-chartjs-2';
+import { CategoryScale } from "chart.js";
+import { useState } from 'react';
+export const Data = [
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000
+   
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 45677
+   
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 78888
+   
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 90000
+   
+  },
+  {
+    id: 5,
+    year: 2020,
+    userGain: 4300
+   
+  }
+];
 
 function App() {
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data) => data.year), 
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: Data.map((data) => data.userGain),
+       
+       
+      }
+    ]
+  }) ;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Line
+        data={chartData}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: "Users Gained between 2016-2020"
+            },
+            legend: {
+              display: false
+            },
+            
+
+          }
+        }}
+      />
+
+
     </div>
   );
 }
